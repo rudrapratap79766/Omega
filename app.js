f712c575af999d18ca40d7e9268202fb27d6d95c
rudrapratap79766 <69892409+rudrapratap79766@users.noreply.github.com>
2020-08-20 13:40:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
 ///Edit Under This only
 
-var const firebaseConfig = {
+ const firebaseConfig = {
   apiKey: "AIzaSyCYxs9x8xQWlyPPtHRf8hag8nTKCl-2ia4",
   authDomain: "hacking-48c5b.firebaseapp.com",
   databaseURL: "https://hacking-48c5b.firebaseio.com",
@@ -13,7 +13,7 @@ var const firebaseConfig = {
   appId: "1:590282575068:web:3e77b80afd6738bc15f819",
   measurementId: "G-3XC3NFHYFV"
 };
-};
+
 
      
  
