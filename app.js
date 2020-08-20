84f051d942e86549597c512fdbbf35561b966606
rudrapratap79766 <69892409+rudrapratap79766@users.noreply.github.com>
2020-08-20 11:19:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,16 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+var const firebaseConfig = {
+  apiKey: "AIzaSyCYxs9x8xQWlyPPtHRf8hag8nTKCl-2ia4",
+  authDomain: "hacking-48c5b.firebaseapp.com",
+  databaseURL: "https://hacking-48c5b.firebaseio.com",
+  projectId: "hacking-48c5b",
+  storageBucket: "hacking-48c5b.appspot.com",
+  messagingSenderId: "590282575068",
+  appId: "1:590282575068:web:3e77b80afd6738bc15f819",
+  measurementId: "G-3XC3NFHYFV"
+};
 };
 
      
